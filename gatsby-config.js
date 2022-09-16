647f263ff3959d4cfa25b0c0d24e0b0e938f039d
hadwin <73227570+hadwinn@users.noreply.github.com>
2022-09-16 08:43:16 +0800

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ module.exports = {
   pathPrefix: '/running_page', // Change to `/running_page` when running on github pages
   siteMetadata: {
     siteTitle: 'Running Page',
-    siteUrl: 'https://hadwinn.github.io/running_page/',
+    siteUrl: 'https://hadwinn.github.io',
     logo: 'https://cdn.staticaly.com/gh/hadwinn/blog@master/img/running_pace_logo.PNG',
     description: 'Personal site and blog',
     navLinks: [

510e4861182622d072b5bf8a9e3ca146da65bde6
hadwin <73227570+hadwinn@users.noreply.github.com>
2022-09-17 14:58:35 +0800

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ module.exports = {
   siteMetadata: {
     siteTitle: 'Running Page',
     siteUrl: 'https://hadwinn.github.io/running_page/',
-    logo: 'https://cdn.staticaly.com/gh/hadwinn/blog@master/img/running_pace_logo.PNG',
+    logo: 'https://cdn.staticaly.com/gh/hadwinn/blog@master/img/头像1.PNG',
     description: 'Personal site and blog',
     navLinks: [
       {
